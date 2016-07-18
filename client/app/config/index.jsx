6a6ec598d71f45d51862b19a9fe15f7960ae6a47
Radosław Kazaniszyn <rkazaniszyn@squiz.pl>
2016-07-18 14:56:06 +0200

    test

--- client/app/config/index.jsx
+++ client/app/config/index.jsx
@@ -1,10 +1,10 @@
 let config = {}
 switch (process.env.NODE_ENV) {
     case 'development' :
-        config.apiUrl = 'http://localhost:5000/api/v1/';
+        config.apiUrl = 'http://localhost:5000/api/v1';
         break;
     case 'production' :
-        config.apiUrl = 'https://safe-citadel-31187.herokuapp.com/api/v1/';
+        config.apiUrl = 'https://safe-citadel-31187.herokuapp.com/api/v1';
         break;
 }
 
