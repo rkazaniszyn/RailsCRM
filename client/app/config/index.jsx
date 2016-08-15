fc39987ab6014ffd0d4c9521a58cfb62fe5bab56
Radosław Kazaniszyn <rkazaniszyn@squiz.pl>
2016-08-15 20:15:48 +0200

    detect https

--- client/app/config/index.jsx
+++ client/app/config/index.jsx
@@ -1,10 +1,10 @@
 let config = {}
 switch (process.env.NODE_ENV) {
     case 'development' :
-        config.apiUrl = 'http://localhost:5000/api/v1';
+        config.apiUrl = document.location.protocol+'//localhost:5000/api/v1';
         break;
     case 'production' :
-        config.apiUrl = 'https://radekcrm.herokuapp.com/api/v1';
+        config.apiUrl = document.location.protocol+'//radekcrm.herokuapp.com/api/v1';
         break;
 }
 
